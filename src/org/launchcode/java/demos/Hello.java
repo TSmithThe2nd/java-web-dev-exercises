db66f076a5388d3c9c947ae6490dfb6872853fe6
smity51 <smity51@gmail.com>
2022-01-24 16:21:18 -0500

    "Experiment with string methods"

--- src/org/launchcode/java/demos/Hello.java
+++ src/org/launchcode/java/demos/Hello.java
@@ -5,6 +5,26 @@ package org.launchcode.java.demos;
  */
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello, World");
+        String greeting="Hello, World";
+        String var=" Launch Code";
+
+        System.out.println(greeting);
+
+        System.out.println("should print var w/o white space in front "+ var.trim());
+//    Experimenting with stacking
+        var=var.substring(var.indexOf('L'),var.indexOf('h')+1) +var.substring(var.indexOf('C'),var.indexOf('e')+1);
+
+        System.out.println("Should print var with no white space "+var);
+
+        var=var.toUpperCase();
+
+        System.out.println("should print all caps"+ var);
+
+        System.out.println(var.charAt(5));
+
+
+
     }
 }
+
+
