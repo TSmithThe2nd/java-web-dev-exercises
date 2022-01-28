5a8b2405a6834d36d66ba5426d539d5d32280366
smity51 <smity51@gmail.com>
2022-01-28 14:47:14 -0500

    adds control flow module and character counter class, from studio chapter 3

--- src/control/flow/studio/Charater/Counter.java
+++ src/control/flow/studio/Charater/Counter.java
@@ -1,2 +1,33 @@
-package control.flow.studio.Charater;public class Counter {
+package control.flow.studio.Charater;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Arrays;
+
+public class Counter {
+    public static void main(String[] args){
+
+        String quote="If the product of two terms is zero then common sense says at least one of the two terms has to be zero to start with. So if you move all the terms over to one side, you can put the quadratics into a form that can be factored allowing that side of the equation to equal zero. Once you’ve done that, it’s pretty straightforward from there.";
+//        creates hashMap
+        HashMap<String, Integer> quoteMap= new HashMap<>();
+//        splits string into characters in an array
+        String[] charactersInString = quote.split("");
+//  goes through array and adds to hashmap
+        for (String c: charactersInString) {
+            if(!quoteMap.containsKey(c)){
+               quoteMap.put(c,1);
+//           if character is already in hashMap add +1 to count
+            }else{quoteMap.put(c, quoteMap.get(c)+1);
+            }
+
+        }
+//        returns each key value pair
+        for(Map.Entry<String, Integer> keyValue: quoteMap.entrySet()){
+            System.out.println(keyValue.getKey()+":"+keyValue.getValue());
+        }
+
+
+
+
+
+    }
 }
